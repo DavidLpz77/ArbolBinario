550986c3486ce80506ff628d50ba1c2a53cf7544
DavidLpz77 <42592245+DavidLpz77@users.noreply.github.com>
2019-05-27 17:30:52 -0500

    Add files via upload

---
+++ ArbolBinario.py
@@ -0,0 +1,38 @@
+class nodo:
+    izq , der, dato = None, None, 0
+ 
+    def __init__(self, dato):
+        self.izq = None
+        self.der = None
+        self.dato = dato
+ 
+class arbolBinario:
+    def __init__(self):
+        self.raiz = None
+ 
+    def agregarNodo(self, dato):
+        return nodo(dato)
+ 
+    def insertar(self, raiz, dato):
+        if raiz == None:
+            return self.agregarNodo(dato)
+        else:
+            if dato <= raiz.dato:
+                raiz.izq = self.insertar(raiz.izq, dato)
+            else:
+                raiz.der = self.insertar(raiz.der, dato)
+            return raiz
+
+    def buscar(self,nodo,valor):
+        if nodo==None:
+            return False
+        elif nodo.valor==valor:
+            return nodo
+        elif nodo.valor>=valor:
+            return self.buscar(nodo.izq,valor)
+        else:
+            return self.buscar(nodo.der,valor)
+        
+
+
+
